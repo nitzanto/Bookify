f0e49ded8fc9786ca01a30180aaaa2bac53345f1
nitzanto <nitzan16001@gmail.com>
2023-09-28 21:46:33 +0300

    * Added Reservations Repo to inject in services

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
 import { DatabaseModule } from '@app/common';
+import { ReservationsRepository } from './reservations.repository';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [ReservationsController],
-  providers: [ReservationsService],
+  providers: [ReservationsService, ReservationsRepository],
 })
 export class ReservationsModule {}
