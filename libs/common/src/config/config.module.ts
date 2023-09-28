45e442310af31c6993d72405fec8a9c4737496e1
nitzanto <nitzan16001@gmail.com>
2023-09-28 17:10:59 +0300

    * Installed config addon * Setting up DB & Config modules

---
+++ libs/common/src/config/config.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule as NestConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [NestConfigModule.forRoot()],
+})
+export class ConfigModule {}
