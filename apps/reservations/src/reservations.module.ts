096c5de79c9a8124182be8b01238acf185740444
nitzanto <nitzan16001@gmail.com>
2023-09-28 21:50:17 +0300

    * Importing the reservation schema for DB

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -3,9 +3,21 @@ import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
 import { DatabaseModule } from '@app/common';
 import { ReservationsRepository } from './reservations.repository';
+import {
+  ReservationDocument,
+  ReservationSchema,
+} from './models/reservation.schema';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      {
+        name: ReservationDocument.name,
+        schema: ReservationSchema,
+      },
+    ]),
+  ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
 })
