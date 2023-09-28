69d92266d175be2a4c075738591bda7daef7f334
nitzanto <nitzan16001@gmail.com>
2023-09-29 00:10:09 +0300

    * CRUD service

--- apps/reservations/src/reservations.controller.ts
+++ apps/reservations/src/reservations.controller.ts
@@ -27,7 +27,7 @@ export class ReservationsController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.reservationsService.findOne(+id);
+    return this.reservationsService.findOne(id);
   }
 
   @Patch(':id')
@@ -35,11 +35,11 @@ export class ReservationsController {
     @Param('id') id: string,
     @Body() updateReservationDto: UpdateReservationDto,
   ) {
-    return this.reservationsService.update(+id, updateReservationDto);
+    return this.reservationsService.update(id, updateReservationDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.reservationsService.remove(+id);
+    return this.reservationsService.remove(id);
   }
 }
