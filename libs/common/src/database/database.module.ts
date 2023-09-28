5d923c499887e1fa75725672824870fe64ea02f6
nitzanto <nitzan16001@gmail.com>
2023-09-28 17:18:47 +0300

    * Configured db connection

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
-@Module({})
+@Module({
+  imports: [MongooseModule.forRoot('mongodb://localhost/bookify')],
+})
 export class DatabaseModule {}
