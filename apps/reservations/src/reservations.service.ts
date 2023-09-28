e73857123c707a41fe660ad59bf7cad172359950
nitzanto <nitzan16001@gmail.com>
2023-09-28 21:08:39 +0300

    * Refactored reservations microservice
    * CRUD added

--- apps/reservations/src/reservations.service.ts
+++ apps/reservations/src/reservations.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { CreateReservationDto } from './reservations/dto/create-reservation.dto';
+import { UpdateReservationDto } from './reservations/dto/update-reservation.dto';
 
 @Injectable()
 export class ReservationsService {
-  getHello(): string {
-    return 'Hello World!';
+  create(createReservationDto: CreateReservationDto) {
+    return 'This action adds a new reservation';
+  }
+
+  findAll() {
+    return `This action returns all reservations`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} reservation`;
+  }
+
+  update(id: number, updateReservationDto: UpdateReservationDto) {
+    return `This action updates a #${id} reservation`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} reservation`;
   }
 }
