58fe433e67807186bb595033b55e98f1283c0eaf
nitzanto <nitzan16001@gmail.com>
2023-09-29 17:03:12 +0300

    * Added validation for DTO * Whitelist for proper DTO

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,9 +1,17 @@
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateReservationDto {
+  @IsDate()
   startDate: Date;
 
+  @IsDate()
   endDate: Date;
 
+  @IsString()
+  @IsNotEmpty()
   placeId: string;
 
+  @IsString()
+  @IsNotEmpty()
   invoiceId: string;
 }
--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -5,7 +5,7 @@ import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
   await app.listen(3000);
 }
