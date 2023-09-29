94ed430a455fecdce54f0b242eeaf59beb58e846
nitzanto <nitzan16001@gmail.com>
2023-09-29 17:04:37 +0300

    * Added validation for DTO * Whitelist for proper DTO * Tranforming to Date

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,10 +1,13 @@
 import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class CreateReservationDto {
   @IsDate()
+  @Type(() => Date)
   startDate: Date;
 
   @IsDate()
+  @Type(() => Date)
   endDate: Date;
 
   @IsString()
