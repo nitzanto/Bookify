04ee00009619554d944b7538146a3b6f6c0fafc7
nitzanto <nitzan16001@gmail.com>
2023-09-30 22:51:11 +0300

    * Updated user service

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  async create(createUserDto: CreateUserDto) {}
+}
