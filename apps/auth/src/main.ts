a3a05dfa7ee813e0a42db90b33fb367d78dd7276
nitzanto <nitzan16001@gmail.com>
2023-09-30 23:10:24 +0300

    * Added validation to auth

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useLogger(app.get(Logger));
   await app.listen(3001);
 }
+
 bootstrap();
