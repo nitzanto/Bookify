fd37937cfec03aa785cabf47feb0cf52ffc30e70
nitzanto <nitzan16001@gmail.com>
2023-09-30 22:53:43 +0300

    * Importing the db module into users

--- apps/auth/src/users/users.module.ts
+++ apps/auth/src/users/users.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { DatabaseModule } from '@app/common';
+import { UserDocument, UserSchema } from './models/user.schema';
 
 @Module({
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      {
+        name: UserDocument.name,
+        schema: UserSchema,
+      },
+    ]),
+  ],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class UsersModule {}
