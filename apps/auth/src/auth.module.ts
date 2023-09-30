55b89dc743ccde8de5a1cc6cb3a12014019d4268
nitzanto <nitzan16001@gmail.com>
2023-10-01 00:51:42 +0300

    * Validation for ports

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -17,6 +17,7 @@ import * as Joi from 'joi';
         MONGODB_URI: Joi.string().required(),
         JWT_SECRET: Joi.string().required(),
         JWT_EXPIRATION: Joi.string().required(),
+        PORT: Joi.string().required(),
       }),
     }),
     JwtModule.registerAsync({
--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -24,6 +24,7 @@ import * as Joi from 'joi';
       isGlobal: true,
       validationSchema: Joi.object({
         MONGODB_URI: Joi.string().required(),
+        PORT: Joi.string().required(),
       }),
     }),
   ],
