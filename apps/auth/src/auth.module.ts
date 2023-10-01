4c2370717be6eb9687c2635dd5dd91f3796d0519
nitzanto <nitzan16001@gmail.com>
2023-10-01 15:16:14 +0300

    * LocalStrategy added as provider for injecting the class

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -6,6 +6,7 @@ import { LoggerModule } from '@app/common';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
   imports: [
@@ -31,6 +32,6 @@ import * as Joi from 'joi';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
