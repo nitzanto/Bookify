2c5321468c73ddfe29ed5c4573a69208b0fe2e4d
nitzanto <nitzan16001@gmail.com>
2023-10-01 16:00:51 +0300

    * Implemented JWT strategy

---
+++ apps/auth/src/strategies/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { UsersService } from '../users/users.service';
+import { Request } from 'express';
+import { TokenPayload } from '../interfaces/token-payload.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    configService: ConfigService,
+    private readonly usersService: UsersService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => request?.cookies?.Authentication,
+      ]),
+      secretOrKey: configService.get('JWT_SECRET'),
+    });
+  }
+
+  async validate({ userId }: TokenPayload) {}
+}
