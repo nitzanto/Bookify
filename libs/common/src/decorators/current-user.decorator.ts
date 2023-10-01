9cc9fd4cfd6c58b1935d30b7f3170dad84b4bf2b
nitzanto <nitzan16001@gmail.com>
2023-10-01 18:17:56 +0300

    * Moved to lib common for re-useability

--- libs/common/src/decorators/current-user.decorator.ts
+++ libs/common/src/decorators/current-user.decorator.ts
@@ -1,5 +1,5 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { UserDocument } from './users/models/user.schema';
+import { UserDocument } from '../../../../apps/auth/src/users/models/user.schema';
 
 // Decorator used to pull the user object from the request object data
 const getCurrentUserByContext = (context: ExecutionContext): UserDocument => {
