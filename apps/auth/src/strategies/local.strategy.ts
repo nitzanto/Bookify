c88b82879ba2c199fed6833e6967ee15d9b82215
nitzanto <nitzan16001@gmail.com>
2023-10-01 13:54:29 +0300

    * Refactored the directory location

--- apps/auth/src/strategies/local.strategy.ts
+++ apps/auth/src/strategies/local.strategy.ts
@@ -1,7 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
-import { UsersService } from '../src/users/users.service';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
