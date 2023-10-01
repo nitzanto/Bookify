9638d4e50f5d74afeb99263353fb20ca01b66bf1
nitzanto <nitzan16001@gmail.com>
2023-10-01 16:00:41 +0300

    * Added tokenpayload interface

--- apps/auth/src/auth.service.ts
+++ apps/auth/src/auth.service.ts
@@ -3,6 +3,7 @@ import { UserDocument } from './users/models/user.schema';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { Response } from 'express';
+import { TokenPayload } from './interfaces/token-payload.interface';
 
 @Injectable()
 export class AuthService {
@@ -12,7 +13,7 @@ export class AuthService {
   ) {}
 
   async login(user: UserDocument, response: Response) {
-    const tokenPayLoad = {
+    const tokenPayLoad: TokenPayload = {
       userId: user._id.toHexString(),
     };
 
