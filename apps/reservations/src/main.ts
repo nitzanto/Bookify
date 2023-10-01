ba5120685408de7bf796dbd0701199dce399c665
nitzanto <nitzan16001@gmail.com>
2023-10-01 17:45:47 +0300

    * Added cookie parser for JWT cookies in reservations

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -3,11 +3,13 @@ import { ReservationsModule } from './reservations.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 import { ConfigService } from '@nestjs/config';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
+  app.use(cookieParser());
   const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT'));
 }
