f6bd05195d45922e9fec3e330e402c5c1861e4e9
nitzanto <nitzan16001@gmail.com>
2023-10-01 12:27:53 +0300

    * Adding strategy for auth, user's password & email

---
+++ apps/auth/strategies/local.strategy.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {}
