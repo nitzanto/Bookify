1d6a017c4cc8b71b3920121cc2f6c4e52fb88932
nitzanto <nitzan16001@gmail.com>
2023-10-01 17:49:22 +0300

    * Fixed TCP connection between microservices issue
    * Added 2 new ports to env
    -HTTP_PORT
    -TCP_PORT

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -8,13 +8,19 @@ import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
-  app.connectMicroservice({ transport: Transport.TCP });
+  const configService = app.get(ConfigService);
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: configService.get('TCP_PORT'),
+    },
+  });
   app.use(cookieParser()); // all requests with cookies will go through this and be parsed
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
-  const configService = app.get(ConfigService);
   await app.startAllMicroservices();
-  await app.listen(configService.get('PORT'));
+  await app.listen(configService.get('HTTP_PORT'));
 }
 
 bootstrap();
