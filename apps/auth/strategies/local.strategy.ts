e886f5608d7f9e20cefded53507e4421ea47f2cb
nitzanto <nitzan16001@gmail.com>
2023-10-01 12:30:31 +0300

    * Strategy to validate user email and password

--- apps/auth/strategies/local.strategy.ts
+++ apps/auth/strategies/local.strategy.ts
@@ -1,6 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
+import { UsersService } from '../src/users/users.service';
 
 @Injectable()
-export class LocalStrategy extends PassportStrategy(Strategy) {}
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly usersService: UsersService) {
+    super({ usernameField: 'email' });
+  }
+
+  // Validating the user
+  async validate(email: string, password: string) {
+    return this.usersService.validateUser(email, password);
+  }
+}
