3422ab5d3a8ee699d721e33a990238070aaf66ad
nitzanto <nitzan16001@gmail.com>
2023-10-01 16:01:49 +0300

    * Implemented JWT strategy

--- apps/auth/src/strategies/jwt.strategy.ts
+++ apps/auth/src/strategies/jwt.strategy.ts
@@ -20,5 +20,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate({ userId }: TokenPayload) {}
+  async validate({ userId }: TokenPayload) {
+    return this.usersService.getUser({ _id: userId });
+  }
 }
