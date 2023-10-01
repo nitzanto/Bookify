acb581d0b40155f550d67ee15d8cd5e4519fb842
nitzanto <nitzan16001@gmail.com>
2023-10-01 16:53:00 +0300

    * Made the application hybrid. * Currently communication between microservices are based on The TCP protocol

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -4,12 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 import { ConfigService } from '@nestjs/config';
 import * as cookieParser from 'cookie-parser';
+import { Transport } from '@nestjs/microservices';
+
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
+  app.connectMicroservice({ transport: Transport.TCP });
   app.use(cookieParser()); // all requests with cookies will go through this and be parsed
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
   const configService = app.get(ConfigService);
+  await app.startAllMicroservices();
   await app.listen(configService.get('PORT'));
 }
 
