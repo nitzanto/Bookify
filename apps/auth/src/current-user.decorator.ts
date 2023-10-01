b2632437d3c23f7f7d6de518ba21c77ecb3c7e07
nitzanto <nitzan16001@gmail.com>
2023-10-01 14:26:20 +0300

    * Pulling the user data

--- apps/auth/src/current-user.decorator.ts
+++ apps/auth/src/current-user.decorator.ts
@@ -2,8 +2,9 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { UserDocument } from './users/models/user.schema';
 
 // Decorator used to pull the user object from the request object data
-
-const getCurrentUserByContext = (context: ExecutionContext): UserDocument => {};
+const getCurrentUserByContext = (context: ExecutionContext): UserDocument => {
+  return context.switchToHttp().getRequest().user;
+};
 
 export const CurrentUser = createParamDecorator(
   (_data: unknown, context: ExecutionContext) =>
