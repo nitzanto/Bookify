68cc8bbee10f05f2c4bbfb9f1f2fb87d34144933
nitzanto <nitzan16001@gmail.com>
2023-10-01 17:21:49 +0300

    * switched requests to handle both express & rpc call

--- apps/auth/src/strategies/jwt.strategy.ts
+++ apps/auth/src/strategies/jwt.strategy.ts
@@ -3,7 +3,6 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
 import { UsersService } from '../users/users.service';
-import { Request } from 'express';
 import { TokenPayload } from '../interfaces/token-payload.interface';
 
 @Injectable()
@@ -12,9 +11,11 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     configService: ConfigService,
     private readonly usersService: UsersService,
   ) {
+    // request could be from express or an RPC call
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
-        (request: Request) => request?.cookies?.Authentication,
+        (request: any) =>
+          request?.cookies?.Authentication || request?.Authentication,
       ]),
       secretOrKey: configService.get('JWT_SECRET'),
     });
