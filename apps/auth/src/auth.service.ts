00b923f08b5cf459e7ea49b844f83c6b91a6dce2
nitzanto <nitzan16001@gmail.com>
2023-10-01 15:14:47 +0300

    * Generation of HTTP Cookies and JWT token for the user

--- apps/auth/src/auth.service.ts
+++ apps/auth/src/auth.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { UserDocument } from './users/models/user.schema';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { Response } from 'express';
 
 @Injectable()
 export class AuthService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(user: UserDocument, response: Response) {
+    const tokenPayLoad = {
+      userId: user._id.toHexString(),
+    };
+
+    const expires = new Date();
+    expires.setSeconds(
+      expires.getSeconds() + this.configService.get('JWT_EXPIRATION'),
+    );
+
+    // Generating the JWT token for the user
+    const token = this.jwtService.sign(tokenPayLoad);
+
+    // Securing the cookie for HTTP requests only without client-side
+    response.cookie('Authentication', token, {
+      httpOnly: true,
+      expires,
+    });
   }
 }
