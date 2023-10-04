4557aa8506b0932bdc229789c257799c8f769a5c
nitzanto <nitzan16001@gmail.com>
2023-10-04 14:45:23 +0300

    * Setting up the stripe client

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,7 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import Stripe from 'stripe';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PaymentsService {
+  constructor(private readonly configService: ConfigService) {}
+
+  private readonly stripe = new Stripe(
+    this.configService.get('STRIPE_SECRET_KEY'),
+    {
+      apiVersion: '2023-08-16',
+    },
+  );
+
   getHello(): string {
     return 'Hello World!';
   }
