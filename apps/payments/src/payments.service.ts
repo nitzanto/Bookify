97121306d1660bf50fa7197464ad48752c8250c3
nitzanto <nitzan16001@gmail.com>
2023-10-04 14:54:15 +0300

    * Created charging payments in payments services with the stripe API

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import Stripe from 'stripe';
 import { ConfigService } from '@nestjs/config';
+import { CreateChargeDto } from './dto/create-charge.dto';
 
 @Injectable()
 export class PaymentsService {
@@ -13,7 +14,20 @@ export class PaymentsService {
     },
   );
 
-  getHello(): string {
-    return 'Hello World!';
+  async createCharge({ card, amount }: CreateChargeDto) {
+    const paymentMethod = await this.stripe.paymentMethods.create({
+      type: 'card',
+      card,
+    });
+
+    const paymentIntent = await this.stripe.paymentIntents.create({
+      payment_method: paymentMethod.id,
+      amount: amount * 100,
+      confirm: true,
+      payment_method_types: ['card'],
+      currency: 'usd',
+    });
+
+    return paymentIntent;
   }
 }
