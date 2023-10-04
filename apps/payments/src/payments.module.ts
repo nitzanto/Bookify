6e2630495e434edc900cffd44bc1155fdd20b4f4
nitzanto <nitzan16001@gmail.com>
2023-10-04 14:25:16 +0300

    * Added Joi validation schema for the microservice payments

--- apps/payments/src/payments.module.ts
+++ apps/payments/src/payments.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().required(),
+      }),
+    }),
+  ],
   controllers: [PaymentsController],
   providers: [PaymentsService],
 })
