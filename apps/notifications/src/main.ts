25bec8f4e34841a54873c965e10cea73dbc17f14
nitzanto <nitzan16001@gmail.com>
2023-10-04 22:05:06 +0300

    * Properly setting up the microservice

--- apps/notifications/src/main.ts
+++ apps/notifications/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from 'nestjs-pino';
 import { NotificationsModule } from './notifications.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(NotificationsModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: configService.get('PORT'),
+    },
+  });
+
+  app.useLogger(app.get(Logger));
+  await app.startAllMicroservices(); // starting the TCP microservice
 }
+
 bootstrap();
