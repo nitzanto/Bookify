db9ad7807345998c0652d7cd945ef58b2ba224c1
nitzanto <nitzan16001@gmail.com>
2023-10-04 14:43:43 +0300

    * Validating API Key

--- apps/payments/src/payments.module.ts
+++ apps/payments/src/payments.module.ts
@@ -11,6 +11,7 @@ import { LoggerModule } from '@app/common';
       isGlobal: true,
       validationSchema: Joi.object({
         PORT: Joi.number().required(),
+        STRIPE_SECRET_KEY: Joi.string().required(),
       }),
     }),
     LoggerModule,
