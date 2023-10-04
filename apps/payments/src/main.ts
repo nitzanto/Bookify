0278b197de566dc51d108b3386c95b3cf7c3f444
nitzanto <nitzan16001@gmail.com>
2023-10-04 14:29:25 +0300

    * Added Logging Formatting from the pino logger

--- apps/payments/src/main.ts
+++ apps/payments/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { PaymentsModule } from './payments.module';
 import { Transport } from '@nestjs/microservices';
 import { ConfigService } from '@nestjs/config';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   // standalone microservice
@@ -15,6 +16,7 @@ async function bootstrap() {
     },
   });
 
+  app.useLogger(app.get(Logger));
   await app.startAllMicroservices(); // starting the TCP microservice
 }
 
