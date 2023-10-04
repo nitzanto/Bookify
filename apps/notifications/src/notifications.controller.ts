c251d82162b879a3b6f224f6e289995f862284af
nitzanto <nitzan16001@gmail.com>
2023-10-04 22:37:29 +0300

    * Event Pattern for microservice

--- apps/notifications/src/notifications.controller.ts
+++ apps/notifications/src/notifications.controller.ts
@@ -1,7 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UsePipes, ValidationPipe } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
+import { EventPattern, Payload } from '@nestjs/microservices';
+import { NotifyEmailDto } from './dto/notify-email.dto';
 
 @Controller()
 export class NotificationsController {
   constructor(private readonly notificationsService: NotificationsService) {}
+
+  @UsePipes(new ValidationPipe())
+  @EventPattern('notify_email')
+  async notifyEmail(@Payload() data: NotifyEmailDto) {
+    await this.notificationsService.notifyEmail(data);
+  }
 }
