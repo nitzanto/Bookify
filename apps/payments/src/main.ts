d8986c26329d2284a8d5b12c4ac95f554ba3dd3f
nitzanto <nitzan16001@gmail.com>
2023-10-04 14:28:35 +0300

    * Implmented it as a standalone microservice

--- apps/payments/src/main.ts
+++ apps/payments/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { PaymentsModule } from './payments.module';
+import { Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  // standalone microservice
   const app = await NestFactory.create(PaymentsModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: configService.get('PORT'),
+    },
+  });
+
+  await app.startAllMicroservices(); // starting the TCP microservice
 }
+
 bootstrap();
