cdfc1885c1622dc0b9e9d0d1700f6f3dc246ba4f
nitzanto <nitzan16001@gmail.com>
2023-10-04 22:07:27 +0300

    * Deleting default controller, service

--- apps/notifications/src/notifications.controller.ts
+++ apps/notifications/src/notifications.controller.ts
@@ -4,9 +4,4 @@ import { NotificationsService } from './notifications.service';
 @Controller()
 export class NotificationsController {
   constructor(private readonly notificationsService: NotificationsService) {}
-
-  @Get()
-  getHello(): string {
-    return this.notificationsService.getHello();
-  }
 }
