88111d574e14947280861ac80e5b0855cd088771
nitzanto <nitzan16001@gmail.com>
2023-10-05 18:00:32 +0300

    * Transport node mailer in service

--- apps/notifications/src/notifications.service.ts
+++ apps/notifications/src/notifications.service.ts
@@ -1,7 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { NotifyEmailDto } from './dto/notify-email.dto';
+import * as nodemailer from 'nodemailer';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class NotificationsService {
+  constructor(private readonly configService: ConfigService) {}
+
+  private readonly transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: this.configService.get('SMTP_USER'),
+      clientId: this.configService.get('GOOGLE_OAuth'),
+      clientSecret: this.configService.get('GOOGLE_OAUTH_CLIENT_SECRET'),
+      refreshToken: this.configService.get('GOOGLE_OAUTH_REFRESH_TOKEN'),
+    },
+  });
+
   async notifyEmail({ email }: NotifyEmailDto) {}
 }
