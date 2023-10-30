abc52c948ef0fbbae197060fa6739900a3e70c51
nitzanto <nitzan16001@gmail.com>
2023-10-30 16:08:47 +0200

    * Added health checks to auth, payments & notifications

--- e2e/specs/healthcheck.app.e2e.spec.ts
+++ e2e/specs/healthcheck.app.e2e.spec.ts
@@ -1,6 +1,31 @@
+import { ping } from 'tcp-ping';
+
 describe('Health', () => {
   test('Reservations', async () => {
     const res = await fetch('http://reservations:3000');
     expect(res.ok).toBeTruthy();
+  });
+
+  test('Auth', async () => {
+    const res = await fetch('http://auth:3001');
+    expect(res.ok).toBeTruthy();
+  });
+
+  test('Payments', async (done) => {
+    ping({ address: 'payments', port: 3003 }, (err) => {
+      if (err) {
+        fail();
+      }
+      done();
+    });
+  });
+
+  test('Notifications', async (done) => {
+    ping({ address: 'notifications', port: 3004 }, (err) => {
+      if (err) {
+        fail();
+      }
+      done();
+    });
   });
 });
