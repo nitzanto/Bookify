1a1f1719251ce7a8a0c10cb3083e12637d349a32
nitzanto <nitzan16001@gmail.com>
2023-11-13 16:37:53 +0200

    * Error handling payments processing

--- apps/payments/src/controllers/payments.controller.ts
+++ apps/payments/src/controllers/payments.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller, UsePipes, ValidationPipe, Res } from '@nestjs/common';
 import { PaymentsService } from '../services/payments.service';
 import {
   Ctx,
@@ -7,6 +7,7 @@ import {
   RmqContext,
 } from '@nestjs/microservices';
 import { PaymentsCreateChargeDto } from '../dto/payments-create-charge.dto';
+import { Response } from 'express';
 
 @Controller()
 export class PaymentsController {
@@ -18,12 +19,19 @@ export class PaymentsController {
   async createCharge(
     @Payload() data: PaymentsCreateChargeDto, // extracts the payload data from the incoming message for further processing.
     @Ctx() context: RmqContext,
+    @Res() res: Response,
   ) {
     const channel = context.getChannelRef();
     const originalMsg = context.getMessage();
 
-    channel.ack(originalMsg); // acknowledges the receipt of the message, confirming its processing.
+    try {
+      channel.ack(originalMsg); // acknowledges the receipt of the message, confirming its processing.
 
-    return this.paymentsService.makePayment(data);
+      return this.paymentsService.makePayment(data);
+    } catch (err) {
+      return res.status(500).json({
+        error: `Payment processing failed: ${err.message}`,
+      });
+    }
   }
 }
