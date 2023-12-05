47f1c24a653ab458df77a7c68f137f065fe2107b
nitzanto <nitzan16001@gmail.com>
2023-12-05 16:45:50 +0200

    * New Notification Message

--- libs/common/src/messages/notificationMessages.ts
+++ libs/common/src/messages/notificationMessages.ts
@@ -19,17 +19,17 @@ export const purchaseNotification = (
         </style>
       </head>
       <body>
-        <div class="invoice-header">
-          <img src="your_logo_url" alt="Bookify Logo" style="max-width: 200px;" />
+        <div class='invoice-header'>
+          <img src='https://i.imgur.com/r92Fdcp.png' alt='Bookify Logo' style='max-width: 200px;' />
         </div>
         <p>Subject: Receipt for Payment at Bookify - Reservation Table</p>
         <p>Your payment of $${amount} for Invoice #${invoiceId} has been successfully processed.</p>
-        <div class="invoice-details">
+        <div class='invoice-details'>
           <p>Invoice ID: ${invoiceId}</p>
         </div>
         <p>Your payment has been received and credited against the outstanding invoice. We appreciate your promptness in settling this transaction.</p>
         <p>Thank you for your business and continued support. We look forward to serving you in the future.</p>
-        <p>If you have any questions regarding this payment, please don't hesitate to contact our support team at <a href="https://bookify.com">bookify.com</a>.</p>
+        <p>If you have any questions regarding this payment, please don't hesitate to contact our support team at <a href='https://bookify.com'>bookify.com</a>.</p>
         <p>Warm regards,<br />Bookify</p>
       </body>
     </html>
